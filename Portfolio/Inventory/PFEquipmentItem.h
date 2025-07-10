5f13a13c06fc7ff9a366fcc2eb37c43de6456ae2
notchcamo <notchcamo@gmail.com>
2025-07-10 16:07:25 +0900

    feat: Add EquipmentItem class

---
+++ Portfolio/Inventory/PFEquipmentItem.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PFInventoryItem.h"
+#include "PFEquipmentItem.generated.h"
+
+UCLASS()
+class PORTFOLIO_API UPFEquipmentItem : public UPFInventoryItem
+{
+    GENERATED_BODY()
+};
